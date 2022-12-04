ed566a313bb29c7bbb1d2fa0febcc43c79b891cc
CVOvidiu <cvovidiu2002@gmail.com>
2022-12-04 19:32:27 +0200

    Refactored javascript codebase

---
+++ README.md
@@ -0,0 +1,10 @@
+# Calculator Project
+
+This is a personal project, written in JavaScript.
+
+## Features
+
+- When a number button is pressed:
+  - concatenate to other digits if they exist.
+  - output with comma
+-
--- script.js
+++ script.js
@@ -1,74 +1,116 @@
-const outputElem = document.querySelector('.calc__output');
-const historyElem = document.querySelector('.calc__history')
-let output = '0';
-let reminder = 0;
+"use strict";
 
-function commaFormat(number) {
+const outputElem = document.querySelector(".calc__output");
+const historyElem = document.querySelector(".calc__history");
+const calcBtns = document.querySelector(".calc__btns");
+
+class App {
+  #output = "0";
+  #reminder = 0;
+
+  constructor() {
+    calcBtns.addEventListener("click", this.#state.bind(this));
+  }
+
+  #state(e) {
+    // Event delegation
+    if (e.target.classList.contains("calc__btns")) return;
+
+    // Button pressed
+    const pressed = e.target.innerHTML;
+
+    // Number button
+    if (e.target.classList.contains("btn--num"))
+      this.#output = this.#numBtnHandler(pressed);
+
+    // Delete button
+    if (e.target.innerHTML === "del") this.#output = this.#delBtnHandler();
+
+    // Dot button
+    if (e.target.innerHTML === ".") this.#output = this.#dotBtnHandler();
+
+    // // Plus button
+    // if (e.target.innerHTML === "+") this.#plusBtnHandler();
+
+    // // Equal button
+    // if (e.target.innerHTML === "=") this.#output = this.#equalBtnHandler();
+
+    outputElem.innerHTML = this.#formatComma(this.#output);
+    console.log(e.target, pressed, this.#output); //! Debugging purposes
+  }
+
+  #numBtnHandler(pressed) {
+    let output = this.#output;
+    // Stop at 15 digits
+    if (output.length === 15) return output;
+
+    // Normal behavior
+    if (output === "0") output = pressed;
+    else output += `${pressed}`;
+
+    // Font-size transformation
+    if (output.length > 10)
+      outputElem.style.fontSize = `${50 + 3.5 * (10 - output.length)}px`;
+
+    return output;
+  }
+
+  #delBtnHandler() {
+    let output = this.#output;
+    // Normal behavior
+    if (output.length === 1) output = "0";
+    else output = output.slice(0, output.length - 1);
+
+    // Font-size transformation
+    if (output.length > 10)
+      outputElem.style.fontSize = `${50 + 3.5 * (10 - output.length)}px`;
+
+    return output;
+  }
+
+  #dotBtnHandler() {
+    let output = this.#output;
+    if (!output.includes(".")) output += ".";
+
+    return output;
+  }
+
+  // #plusBtnHandler() {
+  //   let output = this.#output;
+  //   this.#reminder = Number(output);
+  //   output += " +";
+  //   historyElem.innerHTML = output;
+  //   output = "0";
+  //   this.output = output;
+  // }
+
+  // #equalBtnHandler() {
+  //   historyElem.innerHTML += ` ${output}`;
+  //   output = String(eval(historyElem.innerHTML));
+  //   outputElem.innerHTML = commaFormat(String(eval(historyElem.innerHTML)));
+  // }
+
+  #formatComma(num) {
     // Split the number into integer part and decimal part
-    let [intNumber, decNumber] = number.split('.');
+    let [intNumber, decNumber] = num.split(".");
     intNumber = Number(intNumber);
-    // We take the last three digits from 'intNumber'
+    // Take the last three digits from 'intNumber'
     let format = String(intNumber).slice(-3);
     intNumber = Math.trunc(intNumber / 1000);
 
     // What remains of 'intNumber' but still can be formated
-    while(intNumber > 999) {
-        format = `${String(intNumber).slice(-3)},${format}`;
-        intNumber = Math.trunc(intNumber / 1000);
+    while (intNumber > 999) {
+      format = `${String(intNumber).slice(-3)},${format}`;
+      intNumber = Math.trunc(intNumber / 1000);
     }
 
     // What remains of 'intNumber'
-    if(intNumber) format = `${String(intNumber).slice(-3)},${format}`;
+    if (intNumber) format = `${String(intNumber).slice(-3)},${format}`;
 
-    if(decNumber === '')
-        return `${format}.`;
-    else if(decNumber === undefined)
-        return format;
-    else 
-        return `${format}.${decNumber}`; 
+    if (decNumber === "") return `${format}.`; // If dot button was pressed
+    else if (decNumber === undefined) return format;
+    else return `${format}.${decNumber}`;
+  }
 }
 
-document.querySelector('.calc__btns').addEventListener('click', e => {
-    if(e.target.classList.contains('calc__btns')) return;
-    
-    const pressed = e.target.innerHTML;
-
-    // Number button
-    if(e.target.classList.contains('btn--num')) {
-        if(output === '0') output = pressed;
-        else output += `${pressed}`;
-
-        outputElem.innerHTML = commaFormat(output);
-    }
-
-    // Delete button
-    if(e.target.innerHTML === 'del') {
-        if(output.length === 1) output = '0';
-        else output = output.slice(0, output.length - 1);
-
-        outputElem.innerHTML = commaFormat(output);
-    }
-
-    // Dot button
-    if(e.target.innerHTML === '.') {
-        output += '.';
-        outputElem.innerHTML = commaFormat(output);
-    }
-
-    // +
-    if(e.target.innerHTML === '+') {
-        reminder = Number(output);
-        output += ' +';
-        historyElem.innerHTML = output;
-        output = '0';
-    }
-
-    // =
-    if(e.target.innerHTML === '=') {
-        historyElem.innerHTML += ` ${output}`;
-        output = String(eval(historyElem.innerHTML));
-        outputElem.innerHTML = commaFormat(String(eval(historyElem.innerHTML)));
-    }
-
-    console.log(e.target, pressed, output);
-});
+const instance = new App();
